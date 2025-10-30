0f3c306cab68bd10f797dd8ea0addaa9d650f618
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-10-30 16:22:24 +0300

    feature : add a project
    * Added a project

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("application")
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
