8bfe4c88b53f88dd1b5968a2668ca2270572a100
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-10-30 17:42:49 +0300

    feature : add a greetings user
    * Added a greetings user

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void hello() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String name = in.next();
+        System.out.println("Hello, " + name + "!");
+
+        in.close();
+    }
+}
