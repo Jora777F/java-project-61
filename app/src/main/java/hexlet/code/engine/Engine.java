53d6e50343d3f2f9e341a513860e28e89b75b1c1
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-10-31 17:09:48 +0300

    update : update the engine class
    * Updated the engine class

--- app/src/main/java/hexlet/code/engine/Engine.java
+++ app/src/main/java/hexlet/code/engine/Engine.java
@@ -19,8 +19,8 @@ public class Engine {
                 continue;
             }
 
-            System.out.println("'" + answer + "' is wrong answer ;(. " +
-                    "Correct answer was '" + correctAnswer + "'.");
+            System.out.println("'" + answer + "' is wrong answer ;(. "
+                    + "Correct answer was '" + correctAnswer + "'.");
             System.out.println("Let's try again, " + userName + "!");
             return;
         }
