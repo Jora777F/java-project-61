00d084c92d7ca2b5a0100a96826fb2054523c53d
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-10-31 11:59:54 +0300

    refactor : edit a cli
    * Edited the Command Line Interface

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -8,7 +8,7 @@ public class Cli {
         Scanner in = new Scanner(System.in);
 
         System.out.print("May I have your name? ");
-        String name = in.next();
+        String name = in.nextLine();
         System.out.println("Hello, " + name + "!");
 
         in.close();
