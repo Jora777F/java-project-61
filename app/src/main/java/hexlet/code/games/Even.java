920415057555c1dc2e9e152b40c55c9758417304
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-01 12:42:14 +0300

    feature : edit the even game
    * Edited the even game

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,11 +3,13 @@ package hexlet.code.games;
 import hexlet.code.engine.Engine;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Even {
 
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 100;
+    private static final Random RANDOM = new Random();
 
     public static void checkEven(int iterations, Scanner scanner, String userName) {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
@@ -16,7 +18,7 @@ public class Even {
         String[] correctAnswers = new String[iterations];
 
         for (int i = 0; i < iterations; i++) {
-            int randomNumber = MIN_VALUE + (int) (Math.random() * ((MAX_VALUE - MIN_VALUE) + 1));
+            int randomNumber = RANDOM.nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE;
             questions[i] = String.valueOf(randomNumber);
             correctAnswers[i] = isEvenNumber(randomNumber) ? "yes" : "no";
         }
