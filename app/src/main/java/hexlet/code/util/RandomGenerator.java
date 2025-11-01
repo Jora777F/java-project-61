c2e45214411f4b11a7904df50d4d2180271e3893
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-01 14:27:18 +0300

    feature : add a random generator
    * Added a random generator

---
+++ app/src/main/java/hexlet/code/util/RandomGenerator.java
@@ -0,0 +1,14 @@
+package hexlet.code.util;
+
+import java.security.SecureRandom;
+
+public class RandomGenerator {
+
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static int generateNumber() {
+        return RANDOM.nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE;
+    }
+}
