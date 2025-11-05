3c56fd83e624dfb16dfbfa7695fc4d3da47078c2
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-05 14:46:45 +0300

    refactor : edit the random generator class
    * Edited the random generator

--- app/src/main/java/hexlet/code/util/RandomGenerator.java
+++ app/src/main/java/hexlet/code/util/RandomGenerator.java
@@ -4,13 +4,7 @@ import java.security.SecureRandom;
 
 public class RandomGenerator {
 
-    private static final int MIN_VALUE = 1;
-    private static final int MAX_VALUE = 100;
     private static final SecureRandom RANDOM = new SecureRandom();
-
-    public static int generateNumber() {
-        return RANDOM.nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE;
-    }
 
     public static int generateNumber(int min, int max) {
         return RANDOM.nextInt(max - min + 1) + min;
